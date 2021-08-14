c0cd17cea554e2c737895b12cf53649a91823be9
WSAxel <56919594+WSAxel@users.noreply.github.com>
2021-08-14 17:18:53 +0200

    Added and fixed listing of major financial market indexes in the world.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=US&symbols=%5EIXIC", {
+fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=US&symbols=%5EIXIC%2C%5EGSPC%2C%5EDJI%2C%5EGDAXI%2COSEBN.OL%2C%5EN225", {
     "method": "GET",
     "headers": {
         "x-rapidapi-key": "038fbcf8a8msh4138d84dbdf5692p17be95jsnb9214089f04a",
@@ -8,8 +8,18 @@ fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=
     .then(response => response.json())
     .then(data => {
         console.log(data);
-    })
+        for (let i = 0; i < data['quoteResponse']['result'].length; i++){
+            /*let ul = document.getElementById("ticker")
+            let li = document.createElement("list")
 
-    .catch(err => {
-        console.error(err);
-    });
+            li.appendChild(document.createTextNode(data['quoteResponse']['result'][''+i+'']['shortName']));
+            ul.appendChild(li); */
+            var text;
+            text += "<li>"+data['quoteResponse']['result'][''+i+'']['shortName']+"</li>";
+
+
+        }
+        document.getElementById("ticker").innerHTML=text;
+
+
+    })
