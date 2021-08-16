6c9b8fe7bf0a8b5b001c6477dad9d78a0b5906df
WSAxel <56919594+WSAxel@users.noreply.github.com>
2021-08-16 15:21:57 +0200

    added Plotly.js for making stock chart and implemented alphavantage API for data input to the chart

--- src/app.js
+++ src/app.js
@@ -23,3 +23,30 @@ fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=
         document.getElementById("+/-%").innerHTML=change;
 
     })
+var stockChartValueX = [];
+var stockChartValueY = [];
+/*fetch("https://alpha-vantage.p.rapidapi.com/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=AAPL&outputsize=compact&datatype=json", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "038fbcf8a8msh4138d84dbdf5692p17be95jsnb9214089f04a",
+        "x-rapidapi-host": "alpha-vantage.p.rapidapi.com"
+    }
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        for (var key in data['Time Series (Daily)']){
+            stockChartValueX.push(key);
+            stockChartValueY.push(['Time Series (Daily)'][key]['4. close']);
+        }
+    }) */
+var data = [
+    {
+        x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+        y: [1, 3, 6],
+        type: 'scatter'
+    }
+];
+
+Plotly.newPlot('graph', data);
+
