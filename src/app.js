ce7bf662d7ab0e6ffd53eb7d46912b4abef4a9a3
WSAxel <56919594+WSAxel@users.noreply.github.com>
2021-08-16 19:17:41 +0200

    can't show data from API call on the chart

--- src/app.js
+++ src/app.js
@@ -23,9 +23,9 @@ fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=
         document.getElementById("+/-%").innerHTML=change;
 
     })
-var stockChartValueX = [];
-var stockChartValueY = [];
-/*fetch("https://alpha-vantage.p.rapidapi.com/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=AAPL&outputsize=compact&datatype=json", {
+var stockChartValueXFunction = [];
+var stockChartValueYFunction = [];
+fetch("https://alpha-vantage.p.rapidapi.com/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=AAPL&outputsize=compact&datatype=json", {
     "method": "GET",
     "headers": {
         "x-rapidapi-key": "038fbcf8a8msh4138d84dbdf5692p17be95jsnb9214089f04a",
@@ -36,17 +36,24 @@ var stockChartValueY = [];
     .then(data => {
         console.log(data);
         for (var key in data['Time Series (Daily)']){
-            stockChartValueX.push(key);
-            stockChartValueY.push(['Time Series (Daily)'][key]['4. close']);
+            stockChartValueXFunction.push(key);
+            stockChartValueYFunction.push(data['Time Series (Daily)'][key]['4. close']);
         }
-    }) */
+    })
+console.log(stockChartValueXFunction," X");
+console.log(stockChartValueYFunction, " Y");
+var tull = ["6","5","4","3","3"];
+var tulle = ["5","4","3","2","1"];
 var data = [
     {
-        x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
-        y: [1, 3, 6],
-        type: 'scatter'
+        x: tull,
+        y: tulle,
+        type: 'scatter',
+        mode: 'lines+markers',
+        marker: {color: 'red'},
     }
 ];
+console.log(data, "Chart dataen");
 
 Plotly.newPlot('graph', data);
 
