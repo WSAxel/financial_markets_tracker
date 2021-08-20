180936a98da41732b3202ff12ad9a71a79faed47
WSAxel <56919594+WSAxel@users.noreply.github.com>
2021-08-20 14:46:04 +0200

    finishing header

--- src/app.js
+++ src/app.js
@@ -12,39 +12,63 @@ fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=
         let nasdaqName = data['quoteResponse']['result']['0']['shortName'];
         let nasdaqPercent = data['quoteResponse']['result']['0']['regularMarketChangePercent'];
         nasdaqPercent = nasdaqPercent.toString().slice(0, 5);
+        if (nasdaqPercent <= 0){document.getElementById("nasdaqchange").classList.add("negative")}
+        else {document.getElementById("nasdaqchange").classList.add("positive")}
         let nasdaqPrice = data['quoteResponse']['result']['0']['regularMarketPrice'];
 
         let spName = data['quoteResponse']['result']['1']['shortName'];
         let spPercent = data['quoteResponse']['result']['1']['regularMarketChangePercent'];
-        spPercent = spPercent.toString().slice(0, 5)
+        spPercent = spPercent.toString().slice(0, 5);
+        if (spPercent <= 0){document.getElementById("s&p500change").classList.add("negative")}
+        else {document.getElementById("s&p500change").classList.add("positive")}
         let spPrice = data['quoteResponse']['result']['1']['regularMarketPrice'];
 
         let dowName = data['quoteResponse']['result']['2']['shortName'];
         let dowPercent = data['quoteResponse']['result']['2']['regularMarketChangePercent'];
-        dowPercent = dowPercent.toString().slice(0, 5)
+        dowPercent = dowPercent.toString().slice(0, 5);
+        if (dowPercent <= 0){document.getElementById("dowjoneschange").classList.add("negative")}
+        else {document.getElementById("dowjoneschange").classList.add("positive")}
         let dowPrice = data['quoteResponse']['result']['2']['regularMarketPrice'];
 
         let daxName = data['quoteResponse']['result']['3']['shortName'];
         let daxPercent = data['quoteResponse']['result']['3']['regularMarketChangePercent'];
-        daxPercent = daxPercent.toString().slice(0, 5)
+        daxPercent = daxPercent.toString().slice(0, 5);
+        if (daxPercent <= 0){document.getElementById("daxchange").classList.add("negative")}
+        else {document.getElementById("daxchange").classList.add("positive")}
         let daxPrice = data['quoteResponse']['result']['3']['regularMarketPrice'];
 
         let osbName = data['quoteResponse']['result']['4']['shortName'];
         let osbPercent = data['quoteResponse']['result']['4']['regularMarketChangePercent'];
-        osbPercent = osbPercent.toString().slice(0, 5)
+        osbPercent = osbPercent.toString().slice(0, 5);
+        if (osbPercent <= 0){document.getElementById("osebxchange").classList.add("negative")}
+        else {document.getElementById("osebxchange").classList.add("positive")}
         let osbPrice = data['quoteResponse']['result']['4']['regularMarketPrice'];
 
         let nikName = data['quoteResponse']['result']['5']['shortName'];
         let nikPercent = data['quoteResponse']['result']['5']['regularMarketChangePercent'];
-        osbPercent = osbPercent.toString().slice(0, 5)
+        nikPercent = osbPercent.toString().slice(0, 5);
+        if (nikPercent <= 0){document.getElementById("nikkeichange").classList.add("negative")}
+        else {document.getElementById("nikkeichange").classList.add("positive")}
         let nikPrice = data['quoteResponse']['result']['5']['regularMarketPrice'];
 
-        document.getElementById("nasdaq").innerHTML= nasdaqName+" "+nasdaqPercent+" "+nasdaqPrice;
-        document.getElementById("s&p500").innerHTML= spName+" "+spPercent+" "+spPrice;
-        document.getElementById("dowJones").innerHTML= dowName+" "+dowPercent+" "+dowPrice;
-        document.getElementById("dax").innerHTML= daxName+" "+daxPercent+" "+daxPrice;
-        document.getElementById("osebx").innerHTML= osbName+" "+osbPercent+" "+osbPrice;
-        document.getElementById("nikkei").innerHTML= nikName+" "+nikPercent+" "+nikPrice;
+        document.getElementById("nasdaq").innerHTML= nasdaqName;
+        document.getElementById("nasdaqchange").innerHTML= nasdaqPercent+"%";
+        document.getElementById("nasdaqprice").innerHTML= nasdaqPrice+"";
+        document.getElementById("s&p500").innerHTML= spName;
+        document.getElementById("s&p500change").innerHTML=spPercent+"%";
+        document.getElementById("s&p500price").innerHTML=spPrice;
+        document.getElementById("dowJones").innerHTML= dowName;
+        document.getElementById("dowjoneschange").innerHTML=dowPercent+"%";
+        document.getElementById("dowJonesprice").innerHTML=dowPrice;
+        document.getElementById("dax").innerHTML= daxName;
+        document.getElementById("daxchange").innerHTML=daxPercent+"%";
+        document.getElementById("daxprice").innerHTML=daxPrice;
+        document.getElementById("osebx").innerHTML= osbName;
+        document.getElementById("osebxchange").innerHTML=osbPercent+"%";
+        document.getElementById("osebxprice").innerHTML=osbPrice;
+        document.getElementById("nikkei").innerHTML= nikName;
+        document.getElementById("nikkeichange").innerHTML=nikPercent+"%";
+        document.getElementById("nikkeiprice").innerHTML=nikPrice;
     })
 
 /*fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=US&symbols=%5EIXIC%2C%5EGSPC%2C%5EDJI%2C%5EGDAXI%2COSEBN.OL%2C%5EN225", {
